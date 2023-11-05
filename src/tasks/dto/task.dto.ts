b2f273e461c00317ef9504c959d12e7f04b65dd5
Daniel Uribe <duribegalaz@gmail.com>
2023-11-04 23:46:23 -0300

    class-validator & task validation

--- src/tasks/dto/task.dto.ts
+++ src/tasks/dto/task.dto.ts
@@ -1,12 +1,29 @@
 import { TaskStatus } from "../task.entity"
+import { IsIn, IsNotEmpty, IsOptional, IsString, MinLength } from 'class-validator'
 
 export class CreateTaskDto {
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(4)
     title: string
+
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(7)
     description: string
 }
 
 export class UpdateTaskDto {
+    @IsString()
+    @IsOptional()
     title?: string;
+
+    @IsString()
+    @IsOptional()
     description?: string;
+
+    @IsString()
+    @IsOptional()
+    @IsIn([TaskStatus.PENDING, TaskStatus.IN_PROGRESS, TaskStatus.DONE])
     status?: TaskStatus;
 }
